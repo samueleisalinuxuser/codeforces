33cad3445ca8e2ee3c7b8dbaef685993568a91e0
Samuele <62035219+samueleisalinuxuser@users.noreply.github.com>
2020-11-09 00:31:22 +0100

    Update problem 71A - Way Too Long Words.py

--- problem 71A - Way Too Long Words.py
+++ problem 71A - Way Too Long Words.py
@@ -2,8 +2,8 @@
 A. Way Too Long Words
 time limit per test1 second
 memory limit per test256 megabytes
-inputstandard input
-outputstandard output
+input: standard input
+output: standard output
 
 Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
 Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
