c71fe822ee735ae72851870f4f51f2ee61a01e7f
Samuele <62035219+samueleisalinuxuser@users.noreply.github.com>
2020-11-09 00:28:17 +0100

    Update problem 282A - Word Capitalization.py

--- problem 282A - Word Capitalization.py
+++ problem 282A - Word Capitalization.py
@@ -2,8 +2,8 @@
 A. Word Capitalization
 time limit per test2 seconds
 memory limit per test256 megabytes
-inputstandard input
-outputstandard output
+input: standard input
+output: standard output
 
 Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
 Note, that during capitalization all the letters except the first one remains unchanged.
