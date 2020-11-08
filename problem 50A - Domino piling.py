06755926b09a78cbfa24f0947cae3d2756dee2e1
Samuele <62035219+samueleisalinuxuser@users.noreply.github.com>
2020-11-09 00:30:37 +0100

    Update problem 50A - Domino piling.py

--- problem 50A - Domino piling.py
+++ problem 50A - Domino piling.py
@@ -2,8 +2,8 @@
 A. Domino piling
 time limit per test2 seconds
 memory limit per test256 megabytes
-inputstandard input
-outputstandard output
+input: standard input
+output: standard output
 
 You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares.
 You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
