62dbdb55be236c07172475ae2d90c8a4ba9af54a
Samuele <62035219+samueleisalinuxuser@users.noreply.github.com>
2020-11-09 00:27:03 +0100

    Update problem 271A - Beautiful Year.py

--- problem 271A - Beautiful Year.py
+++ problem 271A - Beautiful Year.py
@@ -2,8 +2,8 @@
 A. Beautiful Year
 time limit per test2 seconds
 memory limit per test256 megabytes
-inputstandard input
-outputstandard output
+input: standard input
+output: standard output
 
 It seems like the year of 2013 came only yesterday. Do you know a curious fact? The year of 2013 is the first year after the old 1987 with only distinct digits.
 Now you are suggested to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
