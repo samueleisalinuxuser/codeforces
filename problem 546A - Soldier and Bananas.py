8d24e615abc5a8a8c3c2c8430f9887684d53de2b
Samuele <62035219+samueleisalinuxuser@users.noreply.github.com>
2020-11-09 00:30:53 +0100

    Update problem 546A - Soldier and Bananas.py

--- problem 546A - Soldier and Bananas.py
+++ problem 546A - Soldier and Bananas.py
@@ -2,8 +2,8 @@
 A. Soldier and Bananas
 time limit per test1 second
 memory limit per test256 megabytes
-inputstandard input
-outputstandard output
+input: standard input
+output: standard output
 
 A soldier wants to buy w bananas in the shop. He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
 He has n dollars. How many dollars does he have to borrow from his friend soldier to buy w bananas?
