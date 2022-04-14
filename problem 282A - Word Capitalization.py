c5d478c9f5396f6740869e4e2d43897c0efa774a
Samuele <62035219+samueleisalinuxuser@users.noreply.github.com>
2022-04-14 09:51:53 +0200

    Update problem 282A - Word Capitalization.py

--- problem 282A - Word Capitalization.py
+++ problem 282A - Word Capitalization.py
@@ -1,31 +1,3 @@
-"""
-A. Word Capitalization
-time limit per test: 2 seconds
-memory limit per test: 256 megabytes
-input: standard input
-output: standard output
-
-Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
-Note, that during capitalization all the letters except the first one remains unchanged.
-
-Input
-A single line contains a non-empty word. This word consists of lowercase and uppercase English letters.
-The length of the word will not exceed 103.
-
-Output
-Output the given word after capitalization.
-
-Examples
-input
-ApPLe
-output
-ApPLe
-input
-konjac
-output
-Konjac
-"""
-
 try:
     w,wl = input(),list()
 
